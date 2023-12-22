f801823b7e8782695b9b605a06b94f3550036d21
Fabien Vauchelles <fabien@vauchelles.com>
2023-12-22 23:41:00 +0100

    chore(website): linter

--- packages/website/components/HomeGetStarted.vue
+++ packages/website/components/HomeGetStarted.vue
@@ -6,10 +6,10 @@ interface IProps {
 defineProps<IProps>();
 
 const code = [
-  "docker run -d -p 8888:8888 -p 8890:8890",
-  "-e AUTH_LOCAL_USERNAME=admin -e AUTH_LOCAL_PASSWORD=password",
-  "-e BACKEND_JWT_SECRET=secret1 -e FRONTEND_JWT_SECRET=secret2",
-  "fabienvauchelles/scrapoxy"
+    "docker run -d -p 8888:8888 -p 8890:8890",
+    "-e AUTH_LOCAL_USERNAME=admin -e AUTH_LOCAL_PASSWORD=password",
+    "-e BACKEND_JWT_SECRET=secret1 -e FRONTEND_JWT_SECRET=secret2",
+    "fabienvauchelles/scrapoxy"
 ];
 
 async function copy($event) {
@@ -47,103 +47,103 @@ async function copy($event) {
 
 <style scoped>
 section {
-  margin-top: 32px;
-  padding: 32px 24px 0;
+    margin-top: 32px;
+    padding: 32px 24px 0;
 
-  border-top: 1px solid var(--vp-c-gutter);
+    border-top: 1px solid var(--vp-c-gutter);
 }
 
 .container {
-  margin: 0 auto;
-  max-width: 1152px;
+    margin: 0 auto;
+    max-width: 1152px;
 }
 
 .header {
-  margin-bottom: 10px;
+    margin-bottom: 10px;
 
-  text-align: center;
+    text-align: center;
 }
 
 .icon {
-  font-size: 20px;
+    font-size: 20px;
 }
 
 .message {
-  padding-top: 10px;
+    padding-top: 10px;
 }
 
 .code {
-  position: relative;
-  max-width: 700px;
-  margin: 0 auto;
-  padding: 24px;
-  overflow: hidden;
+    position: relative;
+    max-width: 700px;
+    margin: 0 auto;
+    padding: 24px;
+    overflow: hidden;
 
-  color: var(--vp-c-text-2);
-  background-color: var(--vp-c-bg-soft);
+    color: var(--vp-c-text-2);
+    background-color: var(--vp-c-bg-soft);
 
-  border-radius: 12px;
+    border-radius: 12px;
 
-  font-family: "Courier New";
+    font-family: "Courier New";
 }
 
 .code:hover button.copy,
 .code:focus button.copy {
-  opacity: 1;
+    opacity: 1;
 }
 
 button.copy {
-  direction: ltr;
-  position: absolute;
-  top: 12px;
-  right: 12px;
-  z-index: 3;
-  border: 1px solid var(--vp-code-copy-code-border-color);
-  border-radius: 4px;
-  width: 40px;
-  height: 40px;
-  background-color: var(--vp-code-copy-code-bg);
-  opacity: 0;
-  cursor: pointer;
-  background-image: var(--vp-icon-copy);
-  background-position: 50%;
-  background-size: 20px;
-  background-repeat: no-repeat;
-  transition: border-color 0.25s, background-color 0.25s, opacity 0.25s;
+    direction: ltr;
+    position: absolute;
+    top: 12px;
+    right: 12px;
+    z-index: 3;
+    border: 1px solid var(--vp-code-copy-code-border-color);
+    border-radius: 4px;
+    width: 40px;
+    height: 40px;
+    background-color: var(--vp-code-copy-code-bg);
+    opacity: 0;
+    cursor: pointer;
+    background-image: var(--vp-icon-copy);
+    background-position: 50%;
+    background-size: 20px;
+    background-repeat: no-repeat;
+    transition: border-color 0.25s, background-color 0.25s, opacity 0.25s;
 }
 
 button.copy:hover {
-  border-color: var(--vp-code-copy-code-hover-border-color);
-  background-color: var(--vp-code-copy-code-hover-bg);
+    border-color: var(--vp-code-copy-code-hover-border-color);
+    background-color: var(--vp-code-copy-code-hover-bg);
 }
 
 button.copy.copied,
 button.copy:hover.copied {
-  border-radius: 0 4px 4px 0;
-  background-color: var(--vp-code-copy-code-hover-bg);
-  background-image: var(--vp-icon-copied);
+    border-radius: 0 4px 4px 0;
+    background-color: var(--vp-code-copy-code-hover-bg);
+    background-image: var(--vp-icon-copied);
 }
 
 button.copy.copied::before,
 button.copy:hover.copied::before {
-  position: relative;
-  top: -1px;
-  transform: translateX(calc(-100% - 1px));
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid var(--vp-code-copy-code-hover-border-color);
-  border-right: 0;
-  border-radius: 4px 0 0 4px;
-  padding: 0 10px;
-  width: fit-content;
-  height: 40px;
-  text-align: center;
-  font-size: 12px;
-  font-weight: 500;
-  color: var(--vp-code-copy-code-active-text);
-  background-color: var(--vp-code-copy-code-hover-bg);
-  white-space: nowrap;
-  content: var(--vp-code-copy-copied-text-content);
+    position: relative;
+    top: -1px;
+    transform: translateX(calc(-100% - 1px));
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid var(--vp-code-copy-code-hover-border-color);
+    border-right: 0;
+    border-radius: 4px 0 0 4px;
+    padding: 0 10px;
+    width: fit-content;
+    height: 40px;
+    text-align: center;
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--vp-code-copy-code-active-text);
+    background-color: var(--vp-code-copy-code-hover-bg);
+    white-space: nowrap;
+    content: var(--vp-code-copy-copied-text-content);
 }
 </style>
