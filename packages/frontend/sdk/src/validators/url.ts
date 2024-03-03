4d3b505350dc561a3ff9a619d4e60eb72283440d
Fabien Vauchelles <fabien@vauchelles.com>
2024-03-03 15:36:06 +0100

    fix(frontend): allow brackets in URL validation

--- packages/frontend/sdk/src/validators/url.ts
+++ packages/frontend/sdk/src/validators/url.ts
@@ -2,7 +2,7 @@ import { AbstractControl } from '@angular/forms';
 import type { ValidationErrors } from '@angular/forms';
 
 
-const URL_PATTERN = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+const URL_PATTERN = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=\[\]]*)$/;
 
 
 export function ValidatorUrl(control: AbstractControl): ValidationErrors | null {
