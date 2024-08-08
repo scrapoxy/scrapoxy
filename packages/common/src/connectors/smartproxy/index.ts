d9597e4ae96ab992e73c2acaeaa88d6ce586efa1
Fabien Vauchelles <fabien@vauchelles.com>
2024-08-08 18:10:29 +0200

    refactor(connectors/smartproxy): remove useless code

--- packages/common/src/connectors/smartproxy/index.ts
+++ packages/common/src/connectors/smartproxy/index.ts
@@ -1,5 +1,3 @@
-export * from './query.interface';
-
 export * from './smartproxy.constants';
 
 export * from './smartproxy.interface';
--- packages/common/src/connectors/smartproxy/query.interface.ts
+++
@@ -1,3 +0,0 @@
-export enum ESmartproxyQueryCredential {
-    Zones = 'zones', // TODO: to clean
-}
