8de81a9c9b12725196a4b6f524c10b9747935dec
Fabien Vauchelles <fabien@vauchelles.com>
2024-08-31 11:53:39 +0200

    fix(python): correct logging level and formatting

--- packages/python-api/src/scrapoxy/scale.py
+++ packages/python-api/src/scrapoxy/scale.py
@@ -2,8 +2,6 @@ from scrapy import signals
 from scrapoxy.api import ScrapoxyApi
 from time import sleep
 
-import logging
-
 
 class ScaleSpiderMiddleware(object):
 
@@ -30,15 +28,15 @@ class ScaleSpiderMiddleware(object):
 
 
     def spider_opened(self, spider):
-        spider.log("[ScaleSpiderMiddleware] Upscale Scrapoxy", level=logging.DEBUG)
+        spider.logger.info("[ScaleSpiderMiddleware] Upscale Scrapoxy")
 
         self._api.set_project_status("HOT")
 
-        spider.log(f"[ScaleSpiderMiddleware] Sleeping {self._delay} seconds to finish upscale", level=logging.WARNING)
+        spider.logger.info("[ScaleSpiderMiddleware] Sleeping %d seconds to finish upscale" % self._delay)
         sleep(self._delay)
 
 
     def spider_closed(self, spider):
-        spider.log("[ScaleSpiderMiddleware] Downscale Scrapoxy", level=logging.DEBUG)
+        spider.logger.info("[ScaleSpiderMiddleware] Downscale Scrapoxy")
 
         self._api.set_project_status("OFF")
