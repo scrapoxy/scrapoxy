e7ce50e46242475200c335cb5b74cd235df35781
Fabien Vauchelles <fabien@vauchelles.com>
2025-01-10 17:42:21 +0100

    refactor(scaleway): reorganize declarations

--- packages/backend/connectors/src/scaleway/scaleway.interface.ts
+++ packages/backend/connectors/src/scaleway/scaleway.interface.ts
@@ -13,6 +13,14 @@ export interface IConnectorScalewayConfig {
     tag: string;
 }
 
+
+export interface IScalewayError {
+    class: string;
+    message: string;
+}
+
+
+//////////// REGIONS ////////////
 export enum EScalewayRegions {
     PAR1 = 'fr-par-1',
     PAR2 = 'fr-par-2',
@@ -25,18 +33,16 @@ export enum EScalewayRegions {
     WAW3 = 'pl-waw-3',
 }
 
-export interface IScalewayError {
-    class: string;
-    message: string;
-}
 
+//////////// VOLUMES ////////////
 export interface IScalewayVolume {
     size: number;
     // eslint-disable-next-line @typescript-eslint/naming-convention
     volume_type: string;
 }
 
-export type IScalewayExtraVolume = IScalewayVolume & {
+
+export interface IScalewayExtraVolume extends IScalewayVolume {
     id: string;
     state: string;
 }
@@ -46,6 +52,37 @@ export interface IScalewayVolumes<T extends IScalewayVolume> {
     [index: string]: T;
 }
 
+
+//////////// NETWORK ////////////
+export interface IScalewayIP {
+    id: string;
+    address: string;
+}
+
+
+export interface IScalewaySecurityRuleRequest {
+    protocol: 'unknown_protocol' | 'TCP' | 'UDP' | 'ICMP' | 'ANY';
+    direction: 'unknown_direction' | 'inbound' | 'outbound';
+    action: 'unknown_action' | 'accept' | 'drop';
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    ip_range: string;
+    position: number;
+    editable: boolean;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    dest_port_from?: number;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    dest_port_to?: number;
+    project: string;
+}
+
+
+export interface IScalewaySecurityRule extends IScalewaySecurityRuleRequest {
+    id: string;
+    zone: string;
+}
+
+
+//////////// INSTANCES ////////////
 export enum EScalewayInstanceState {
     STARTING = 'starting',
     RUNNING = 'running',
@@ -57,10 +94,6 @@ export enum EScalewayInstanceState {
 
 }
 
-export interface IScalewayIP {
-    id: string;
-    address: string;
-}
 
 export interface IScalewayInstanceBase {
     name: string;
@@ -87,6 +120,8 @@ export interface IScalewayCreateInstancesRequest extends IScalewayInstanceBase {
     volumes?: IScalewayVolumes<IScalewayVolume>;
 }
 
+
+//////////// SNAPSHOTS ////////////
 export enum EScalewaySnapshotState {
     AVAILABLE = 'available',
     SNAPSHOTTING = 'snapshotting',
@@ -109,6 +144,8 @@ export interface IScalewaySnapshot {
     state: EScalewaySnapshotState;
 }
 
+
+//////////// IMAGES ////////////
 export enum EScalewayImageState {
     AVAILABLE = 'available',
     CREATING = 'creating',
@@ -120,24 +157,4 @@ export interface IScalewayImage {
     id: string;
     arch: string;
     state: EScalewayImageState;
-}
-
-export interface IScalewaySecurityRuleRequest {
-    protocol: 'unknown_protocol' | 'TCP' | 'UDP' | 'ICMP' | 'ANY';
-    direction: 'unknown_direction' | 'inbound' | 'outbound';
-    action: 'unknown_action' | 'accept' | 'drop';
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    ip_range: string;
-    position: number;
-    editable: boolean;
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    dest_port_from?: number;
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    dest_port_to?: number;
-    project: string;
-}
-
-export interface IScalewaySecurityRule extends IScalewaySecurityRuleRequest {
-    id: string;
-    zone: string;
 }
